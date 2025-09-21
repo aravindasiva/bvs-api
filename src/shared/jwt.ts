081c4e58eea6c7d9a86d1be6ac6ac0193c6f0808
Siva <aravindcva@hotmail.com>
2025-09-21 19:33:57 +0100

    feat: add typing to sign in options

--- src/shared/jwt.ts
+++ src/shared/jwt.ts
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import jwt, { SignOptions } from "jsonwebtoken";
 import { authConfig } from "../config/auth";
 
 type JwtAccessPayload = { sub: string; roles: string[] };
@@ -7,7 +7,7 @@ export function signAccessToken(userId: string, roles: string[]): string {
   return jwt.sign({ sub: userId, roles } as JwtAccessPayload, authConfig.jwtAccessSecret, {
     expiresIn: authConfig.accessTokenTtl,
     algorithm: "HS256",
-  });
+  } as SignOptions);
 }
 
 export function verifyAccessToken(token: string): JwtAccessPayload {
